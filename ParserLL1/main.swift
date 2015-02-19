b389e0ba888bb8f8fdfa0fbea4fd02890b93cab6
volart <dadbaxbax@gmail.com>
2015-02-20 00:59:01 +0300

    Initial Commit

---
+++ ParserLL1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ParserLL1
+//
+//  Created by Artoym Volobuev on 20.02.15.
+//  Copyright (c) 2015 Artoym Volobuev. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
